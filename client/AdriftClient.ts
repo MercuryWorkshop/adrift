539e0fdc6e179d8d4ce05b3db348f3fd47c9743e
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-08-11 16:30:30 -0700

    dev ws trolling

--- client/AdriftClient.ts
+++ client/AdriftClient.ts
@@ -1,7 +1,13 @@
-
+import {
+  BareHeaders,
+  BareResponse,
+  Client,
+  GetRequestHeadersCallback,
+  MetaCallback,
+  ReadyStateCallback,
+  WebSocketImpl,
+} from "bare-client-custom";
 import Connection from "./Connection";
-import { BareClient as BareClientCustom, registerRemoteListener, setBareClientImplementation, Client, GetRequestHeadersCallback, MetaCallback, ReadyStateCallback, WebSocketImpl, BareHeaders, BareResponse } from "bare-client-custom";
-
 
 // export class Adrift {
 //   bareclient:AdriftBareClient,
@@ -14,12 +20,25 @@ export class AdriftBareClient extends Client {
   constructor(private connection: Connection) {
     super();
   }
-  async request(method: string, requestHeaders: BareHeaders, body: BodyInit | null, remote: URL, cache: string | undefined, duplex: string | undefined, signal: AbortSignal | undefined): Promise<BareResponse> {
+  async request(
+    method: string,
+    requestHeaders: BareHeaders,
+    body: BodyInit | null,
+    remote: URL,
+    cache: string | undefined,
+    duplex: string | undefined,
+    signal: AbortSignal | undefined
+  ): Promise<BareResponse> {
     let rawResponse = await this.connection.httprequest({ a: "test data" });
 
     return new Response(JSON.stringify(rawResponse)) as BareResponse;
   }
-  async connect(remote: URL, protocols: string[], getRequestHeaders: GetRequestHeadersCallback, onMeta: MetaCallback, onReadyState: ReadyStateCallback, webSocketImpl: WebSocketImpl): WebSocket {
-
-  }
+  async connect(
+    remote: URL,
+    protocols: string[],
+    getRequestHeaders: GetRequestHeadersCallback,
+    onMeta: MetaCallback,
+    onReadyState: ReadyStateCallback,
+    webSocketImpl: WebSocketImpl
+  ): WebSocket {}
 }
---
+++ client/DevWsTransport.ts
@@ -0,0 +1,27 @@
+import Transport from "./Transport";
+
+export class DevWsTransport extends Transport {
+  ws: WebSocket;
+
+  constructor(onopen, onclose) {
+    super(onopen, onclose);
+
+    this.ws = new WebSocket("ws://localhost:3000/dev-ws");
+    this.ws.binaryType = "arraybuffer";
+    this.ws.onopen = onopen;
+    this.ws.onclose = onclose;
+    this.ws.onmessage = this.onmessage.bind(this);
+  }
+
+  onmessage(msg: MessageEvent<any>) {
+    if (msg.data instanceof ArrayBuffer) {
+      this.ondata(msg.data);
+      return;
+    }
+    // ignore text messages
+  }
+
+  send(data: ArrayBuffer) {
+    this.ws.send(data);
+  }
+}
