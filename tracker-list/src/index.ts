b1bce33c679fe4f3d65d76169ef4e3a4ff7ba309
ProgrammerIn-wonderland <3838shah@gmail.com>
2023-08-19 02:58:22 +0400

    add rafftracker

--- tracker-list/src/index.ts
+++ tracker-list/src/index.ts
@@ -13,15 +13,16 @@ let trackers = {
   },
   "rafftracker": {
     firebase: {
-      apiKey: "AIzaSyCs1LOqsbrAjymIcjvbKxPhFQWXlSPiLTs",
-      authDomain: "adrift-6c1f6.firebaseapp.com",
-      projectId: "adrift-6c1f6",
-      storageBucket: "adrift-6c1f6.appspot.com",
-      messagingSenderId: "175846512414",
-      appId: "1:175846512414:web:5c6e06d231ab58e9029b0f",
-      measurementId: "G-L0P2EF6Q72",
+      apiKey: "AIzaSyDkcda0r-gdiJoTQ7EbOL9q7-NBQwiKlPg",
+      authDomain: "rafftracker.firebaseapp.com",
+      databaseURL: "https://rafftracker-default-rtdb.firebaseio.com",
+      projectId: "rafftracker",
+      storageBucket: "rafftracker.appspot.com",
+      messagingSenderId: "994948039014",
+      appId: "1:994948039014:web:f96970aa4f626e969dc8a7",
+      measurementId: "G-PD96ZKX31D"
     },
-    tracker: "wss://lb1.mercurywork.shop"
+    tracker: "wss://rafftracker.mercurywork.shop/"
   }
 };
 export default trackers;
