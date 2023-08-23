b87e3b2644160772c91ef6a826adce6f9348f30b
Cody <59001842+hard-shutdown@users.noreply.github.com>
2023-08-23 04:17:49 +0000

    recursive dirs

--- server/src/lib.ts
+++ server/src/lib.ts
@@ -3,6 +3,6 @@ import fs from "fs";
 export function datadir(): string {
     let base = (process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.config")) + "/adrift-server"
     if (!fs.existsSync(base))
-        fs.mkdirSync(base);
+        fs.mkdirSync(base, { recursive: true });
     return base;
 }
