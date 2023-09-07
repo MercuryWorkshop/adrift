95895056effe665e9043928d3863b5daabd239c4
Kendell R <KTibow@users.noreply.github.com>
2023-09-07 15:37:14 -0700

    fix gitignore

--- dist/Transport.d.ts
+++
@@ -1,8 +0,0 @@
-export declare abstract class Transport {
-    onopen: () => void;
-    onclose: () => void;
-    ondata: (data: ArrayBuffer) => void;
-    constructor(onopen: () => void, onclose: () => void);
-    abstract send(data: ArrayBuffer): void;
-    abstract close(): void;
-}
--- dist/client/src/DevWsTransport.d.ts
+++
@@ -1,8 +0,0 @@
-import { Transport } from "protocol";
-export declare class DevWsTransport extends Transport {
-    ws: WebSocket;
-    constructor(onopen: () => void, onclose: () => void);
-    onmessage(msg: MessageEvent<any>): void;
-    send(data: ArrayBuffer): void;
-    close(): void;
-}
